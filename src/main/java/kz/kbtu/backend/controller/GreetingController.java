4898d6fd95eeff18e18b85540c8d5c4b42c88d8c
Beisenbek M. Baisakov <b.baisakov@kbtu.kz>
2024-01-20 11:33:38 +0600

    first commit

---
+++ src/main/java/kz/kbtu/backend/controller/GreetingController.java
@@ -0,0 +1,20 @@
+package kz.kbtu.backend.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import kz.kbtu.backend.model.Greeting;
+import kz.kbtu.backend.service.GreetingService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    GreetingService greetingService = new GreetingService();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return greetingService.generateGreeting(name);
+    }
+}
